9ecb9ed34677eac7cc0bbe8934fc8945c86b19d6
Lazare Rossillon <lazare@get-spoke.com>
2024-01-21 15:54:36 +0100

    starting to put data fetching in a separate file, to improve the process

--- sigexport.py
+++ sigexport.py
@@ -11,9 +11,10 @@ import re
 import locale
 
 import click
-from pysqlcipher3 import dbapi2 as sqlcipher
 import markdown
+import uuid
 from bs4 import BeautifulSoup
+from get_data import fetch_data
 
 
 log = False
@@ -186,97 +187,6 @@ def make_simple(dest, conversations, contacts, year=None, attachments_only=False
                 if log:
                     print(f"\t\tNo attachments for a message: {name}, {date_str}")
 
-
-def fetch_data(db_file, key, manual=False, chats=None, conversation_id=None):
-    """Load SQLite data into dicts."""
-
-    contacts = {}
-    convos = {}
-
-    db_file_decrypted = db_file.parents[0] / "db-decrypt.sqlite"
-    if manual:
-        if db_file_decrypted.exists():
-            db_file_decrypted.unlink()
-        command = (
-            f'echo "'
-            f"PRAGMA key = \\\"x'{key}'\\\";"
-            f"ATTACH DATABASE '{db_file_decrypted}' AS plaintext KEY '';"
-            f"SELECT sqlcipher_export('plaintext');"
-            f"DETACH DATABASE plaintext;"
-            f'" | sqlcipher {db_file}'
-        )
-        os.system(command)
-        db = sqlcipher.connect(str(db_file_decrypted))
-        c = db.cursor()
-        c2 = db.cursor()
-    else:
-        db = sqlcipher.connect(str(db_file))
-        c = db.cursor()
-        c2 = db.cursor()
-        # param binding doesn't work for pragmas, so use a direct string concat
-        for cursor in [c, c2]:
-            cursor.execute(f"PRAGMA KEY = \"x'{key}'\"")
-            cursor.execute("PRAGMA cipher_page_size = 4096")
-            cursor.execute("PRAGMA kdf_iter = 64000")
-            cursor.execute("PRAGMA cipher_hmac_algorithm = HMAC_SHA512")
-            cursor.execute("PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA512")
-
-    query = "SELECT type, id, e164, name, profileName, members FROM conversations"
-    if chats is not None:
-        chats = '","'.join(chats)
-        query = query + f' WHERE name IN ("{chats}") OR profileName IN ("{chats}")'
-    c.execute(query)
-    for result in c:
-        if log:
-            print(f"\tLoading SQL results for: {result[3]}")
-        is_group = result[0] == "group"
-        cid = result[1]
-        contacts[cid] = {
-            "id": cid,
-            "name": result[3],
-            "number": result[2],
-            "profileName": result[4],
-            "is_group": is_group,
-        }
-        if contacts[cid]["name"] is None:
-            contacts[cid]["name"] = contacts[cid]["profileName"]
-        convos[cid] = []
-
-        if is_group:
-            usable_members = []
-            # Match group members from phone number to name
-            if result[5] is None:
-                if log:
-                    print("\tEmpty group.")
-            else:
-                for member in result[5].split():
-                    c2.execute(
-                        "SELECT name, profileName FROM conversations WHERE id=?",
-                        [member],
-                    )
-                    for name in c2:
-                        usable_members.append(name[0] if name else member)
-                contacts[cid]["members"] = usable_members
-
-    c.execute("SELECT json, conversationId " "FROM messages " "ORDER BY sent_at")
-    for result in c:
-        content = json.loads(result[0])
-        cid = result[1]
-        if cid and cid in convos:
-            convos[cid].append(content)
-
-    # Insert the new filtering code here
-    if conversation_id is not None:
-        filtered_convos = {}
-        for cid, messages in convos.items():
-            if contacts[cid]["name"] == conversation_id or contacts[cid]["id"] == conversation_id:
-                filtered_convos[cid] = messages
-        return filtered_convos, contacts
-
-    if db_file_decrypted.exists():
-        db_file_decrypted.unlink()
-
-    return convos, contacts
 
 
 def fix_names(contacts):
@@ -636,7 +546,7 @@ def main(
 
     if log:
         print(f"\nFetching data from {db_file}\n")
-    convos, contacts = fetch_data(db_file, key, manual=manual, chats=chats, conversation_id=conversation_id)
+    convos, contacts = fetch_data(db_file, key, manual=manual, chats=chats, conversation_id=conversation_id, log=log)
 
     # ... existing code ...
 
